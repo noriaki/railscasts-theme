8cbc1be4393e08131ae973082c56375e2a28573b
Mike Nichols <mykphyre@yahoo.com>
2015-02-18 09:45:26 -0600

    Adding customizations for railscasts-theme.

--- railscasts-theme.el
+++ railscasts-theme.el
@@ -5,9 +5,9 @@
 ;; Copyright (C) 2009 Oleg Shaldybin <oleg.shaldybin@gmail.com>
 
 ;; Author: Oleg Shaldybin
-;; Adapted-By: Yesudeep Mangalapilly, Yuichi TANIKAWA
+;; Adapted-By: Yesudeep Mangalapilly, Yuichi TANIKAWA, Mike Nichols
 ;; Keywords: railscasts color theme
-;; URL: https://github.com/itiut/railscasts-theme
+;; URL: https://github.com/mikenichols/railscasts-theme
 ;; Requires: GNU Emacs 24
 
 ;; This file is NOT a part of GNU Emacs.
@@ -49,38 +49,66 @@
 
 (custom-theme-set-faces
  'railscasts
+
  '(default ((t (:background "#232323" :foreground "#E6E1DC"))))
- '(cursor ((t (:foreground "#5A647E"))))
+
+ '(cursor ((t (:foreground "#FFFFFF"))))
  '(blue ((t (:foreground "blue"))))
  '(bold ((t (:bold t))))
  '(bold-italic ((t (:italic t :bold t))))
  '(fringe ((t (:background "#232323"))))
- '(font-lock-builtin-face ((t (:foreground "#D0D0FF"))))
- '(font-lock-comment-face ((t (:foreground "#BC9458" :italic t))))
+ '(font-lock-builtin-face ((t (:foreground "#C8C8FF"))))
+ '(font-lock-comment-face ((t (:foreground "#BC9458"))))
  '(font-lock-constant-face ((t (:foreground "#6D9CBE"))))
  '(font-lock-doc-string-face ((t (:foreground "#A5C261"))))
  '(font-lock-function-name-face ((t (:foreground "#FFC66D"))))
  '(font-lock-keyword-face ((t (:foreground "#CC7833"))))
  '(font-lock-preprocessor-face ((t (:foreground "#CC7833"))))
+ '(enh-ruby-op-face ((t (:foreground "#CC7833"))))
  '(font-lock-reference-face ((t (:foreground "LightSteelBlue"))))
  '(font-lock-string-face ((t (:foreground "#A5C261"))))
- '(font-lock-type-face ((t (:foreground "#F2777A"))))
+ '(enh-ruby-string-delimiter-face ((t (:foreground "#A5C261"))))
+ '(font-lock-type-face ((t (:foreground "#6D9CBE"))))
  '(font-lock-variable-name-face ((t (:foreground "LightSteelBlue"))))
  '(font-lock-warning-face ((t (:foreground "Pink"))))
  '(paren-face-match-light ((t (:foreground "#FFC66D" :background "#555577"))))
- '(highlight ((t (:background "darkolivegreen"))))
+ '(show-paren-match ((t (:foreground nil :background "#3070FF"))))
+ '(show-paren-mismatch ((t (:foreground "#00FF00" :background "#FF00FF"))))
+ '(highlight ((t (:background "#2a2a2a"))))
  '(italic ((t (:italic t))))
- '(mode-line ((t (:background "#A5BAF1" :foreground "black" :box nil))))
+ '(mode-line ((t (:background "#CDCDCD" :foreground "black" :box nil))))
  '(modeline-buffer-id ((t (:background "#A5BAF1" :foreground "black"))))
  '(modeline-mousable ((t (:background "#A5BAF1" :foreground "black"))))
  '(modeline-mousable-minor-mode ((t (:background "#A5BAF1" :foreground "black"))))
- '(region ((t (:background "#555577"))))
+ '(region ((t (:foreground "#e0e0e0" :background "#555577"))))
  '(primary-selection ((t (:background "#555577"))))
- '(isearch ((t (:background "#555555"))))
+ '(secondary-selection ((t (:background "DarkSlateBlue"))))
  '(zmacs-region ((t (:background "#555577"))))
- '(secondary-selection ((t (:background "darkslateblue"))))
+
  '(flymake-errline ((t (:background "LightSalmon" :foreground "black"))))
  '(flymake-warnline ((t (:background "LightSteelBlue" :foreground "black"))))
+
+ '(isearch ((t (:foreground "#333333" :background "#22EEEE"))))
+ '(lazy-highlight ((t (:foreground "#333333" :background "#f6e05c"))))
+
+ '(speedbar-button-face ((t (:foreground "#e0e0e0"))))
+ '(speedbar-directory-face ((t (:foreground "#50B0FF"))))
+ '(speedbar-file-face ((t (:foreground "#e0e0e0"))))
+ '(speedbar-highlight-face ((t (:foreground "#FFFFFF" :background "#505050"))))
+ '(speedbar-selected-face ((t (:foreground "#f6e05c"))))
+ '(speedbar-separator-face ((t (:foreground "#e0e0e0" :background "#007f00"))))
+ '(speedbar-tag-face ((t (:foreground "#22a222"))))
+
+ '(wg-mode-line-face ((t (:foreground nil))))
+ '(wg-divider-face ((t (:foreground nil))))
+
+ '(rbenv-active-ruby-face ((t (:foreground "#CC3838"))))
+
+ '(term-color-red ((t (:foreground "#d83b31"))))
+ '(term-color-green ((t (:foreground "#8cf900"))))
+ '(term-color-blue ((t (:foreground "#76a8f0"))))
+ '(term-color-yellow ((t (:foreground "#ffdc40"))))
+
  '(underline ((t (:underline t))))
  '(minibuffer-prompt ((t (:bold t :foreground "#FF6600")))))
 
